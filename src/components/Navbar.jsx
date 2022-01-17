ccfd0b021a84dfd3231e08aecc8e11fa34fe0a1a
James Jefferson <jrj72@Jamess-Mac-mini.local>
2022-01-16 18:46:00 -0600

    input build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,82 @@
+import { Search } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
-const Container = styled.div`
+const Container = styled.div `
   height: 100px;
 `;
-const Wrapper = styled.div`
+const Wrapper = styled.div `
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
-const Left = styled.div``
-const Center = styled.div``
-const Right = styled.div``
+const Left = styled.div `
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
 
+const Language = styled.span `
+  font-size: 14px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div `
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input `
+  border: none;
+`;
+
+const Center = styled.div `
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1 `
+  font-weight: bold;
+`;
+
+const Right = styled.div `
+  flex: 1;
+`;
+
+const MenuItem = styled.div `
+  font-size: 14px;
+  cursor: pointer;
+`;
 
 const Navbar = () => {
-  return (
-    <Container>
-      <Wrapper>
-          <Left>left</Left>
-          <Center>center</Center>
-          <Right>right</Right>
-
-      </Wrapper>
-    </Container>
-  );
+    return ( <
+        Container >
+        <
+        Wrapper >
+        <
+        Left >
+        <
+        Language > EN < /Language>{" "} <
+        SearchContainer >
+        <
+        Input / >
+        <
+        Search / >
+        <
+        /SearchContainer>{" "} <
+        /Left>{" "} <
+        Center > { " " } <
+        Logo > Gearbrokers < /Logo>{" "} <
+        /Center>{" "} <
+        Right >
+        <
+        MenuItem > Register < /MenuItem> <
+        /Right>{" "} <
+        /Wrapper>{" "} <
+        /Container>
+    );
 };
 
 export default Navbar;
