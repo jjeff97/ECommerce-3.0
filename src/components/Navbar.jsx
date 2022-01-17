c58a2d6b049c84edafe2dce3491b6af18a7ecc9d
James Jefferson <jrj72@Jamess-Mac-mini.local>
2022-01-16 23:45:50 -0600

    serach bar set up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -58,23 +59,34 @@ const Navbar = () => {
         <
         Left >
         <
-        Language > EN < /Language>{" "} <
+        Language > EN < /Language> <
         SearchContainer >
         <
         Input / >
         <
         Search / >
         <
-        /SearchContainer>{" "} <
-        /Left>{" "} <
-        Center > { " " } <
-        Logo > Gearbrokers < /Logo>{" "} <
-        /Center>{" "} <
+        /SearchContainer> <
+        /Left> <
+        Center >
+        <
+        Logo > Gearbrokers < /Logo> <
+        /Center> <
         Right >
         <
-        MenuItem > Register < /MenuItem> <
-        /Right>{" "} <
-        /Wrapper>{" "} <
+        MenuItem > REGISTER < /MenuItem> <MenuItem> SIGN IN </MenuItem >
+        <
+        MenuItem >
+        <
+        Badge badgeContent = { 4 }
+        color = "primary" >
+        <
+        ShoppingCartOutlined / >
+        <
+        /Badge> <
+        /MenuItem> <
+        /Right> <
+        /Wrapper> <
         /Container>
     );
 };
