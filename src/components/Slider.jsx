3f0dd40e8184b5d607aa364a0f16b853ecd40413
James Jefferson <jrj72@Jamess-Mac-mini.local>
2022-02-08 23:06:37 -0600

    Hooks setup

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -8,46 +8,42 @@ const Container = styled.div`
 	height: 100vh;
 	display: flex;
 	position: relative;
-	overflow: hidden;
+	
 `;
-const Wrapper = styled.div`
-	height: 100%;
-    display: flex;
-`;
+
 const Slide = styled.div`
-    width: 100vw;
-    height: 100vh;
+	width: 100vw;
+	height: 100vh;
 	display: flex;
 	align-items: center;
-    background-color: #${props=>props.bg};
+	background-color: #${(props) => props.bg};
 `;
 const ImgContainer = styled.div`
-    height:100%;
+	height: 100%;
 	flex: 1;
 `;
 const Image = styled.img`
-    height: 80%;
+	height: 80%;
 `;
 const InfoContainer = styled.div`
 	flex: 1;
-    padding: 50px;
-
+	padding: 50px;
 `;
 
 const Title = styled.h1`
-font-size: 70px;
+	font-size: 70px;
 `;
 const Description = styled.p`
-margin: 50px 0px;
-font-size: 20px;
-font-weight: 500;
-letter-spacing: 3px;
+	margin: 50px 0px;
+	font-size: 20px;
+	font-weight: 500;
+	letter-spacing: 3px;
 `;
 const Button = styled.button`
-padding: 10px;
-font-size: 20px;
-background-color: transparent;
-cursor: pointer;
+	padding: 10px;
+	font-size: 20px;
+	background-color: transparent;
+	cursor: pointer;
 `;
 
 const Arrow = styled.div`
@@ -66,8 +62,19 @@ const Arrow = styled.div`
 	margin: auto;
 	cursor: pointer;
 	opacity: 0.5;
+    z-index: 2;
 `;
+
+const Wrapper = styled.div`
+	height: 100%;
+	display: flex;
+	transform: translatex(0vw);
+`;
+
 const Slider = () => {
+    const [slideIndex, setSlideIndex] = useState(0);
+
+	const handleClick = (direction) => {};
 	return (
 		<Container>
 			<Arrow direction="left" onClick={() => handleClick("left")}>
