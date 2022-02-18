80a1e86f856a40ebefabf25d929c441b52c8c9ed
James Jefferson <jrj72@Jamess-Mac-mini.local>
2022-02-17 18:24:54 -0600

    Catergory styling

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,27 +1,48 @@
+/** @format */
 
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
-flex:1;
-margin: 3px;
+	flex: 1;
+	margin: 3px;
+	height: 70vh;
+	position: relative;
 `;
 const Image = styled.img`
-width: 100%
+	width: 100%;
+	height: 80%;
+	object-fit: cover;
 `;
-const Info = styled.div``;
-const Title = styled.h1``;
+const Info = styled.div`
+	position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+const Title = styled.h1`
+color: orange;
+margin-bottom: 20px;
+
+`;
+
 const Button = styled.button``;
 
- 
-const CategoryItem = ({item}) => {
-    return <Container>
-        <Image src={item.img} />
-        <Info>
-            <Title>{item.title}</Title>
-            <Button>SHOP NOW</Button>
-        </Info>
-    </Container>;
+const CategoryItem = ({ item }) => {
+	return (
+		<Container>
+			<Image src={item.img} />
+			<Info>
+				<Title>{item.title}</Title>
+				<Button>SHOP NOW</Button>
+			</Info>
+		</Container>
+	);
 };
 
 export default CategoryItem;
