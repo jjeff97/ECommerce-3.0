38329098729c444059e0df219d7d674f7245fa5d
James Jefferson <jrj72@Jamess-Mac-mini.local>
2022-02-20 19:09:56 -0600

    Products image build

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,36 @@
+/** @format */
 
-import styled from 'styled-components';
-import { popularProducts } from '../data';
+import { FavoriteBorderOutlined, SearchOutlined, ShoppingBasketOutlined } from "@material-ui/icons";
+import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+    flex:1;
+    margin: 5px;
+    `;
 
-const Product = () => {
+const Circle = styled.div`
+
+`;
+
+
+const Product = ({ item }) => {
     return (
-    <Container>
-            {popularProducts.map(item => (
-                <Product item={item}/>
-    ))}
-    
-        </Container>
-    )
+			<Container>
+				<Circle />
+				<Image src={item.img} />
+				<Info>
+					<Icon>
+						<ShoppingBasketOutlined />
+					</Icon>
+					<Icon>
+						<SearchOutlined />
+					</Icon>
+					<Icon>
+						<FavoriteBorderOutlined />
+					</Icon>
+				</Info>
+			</Container>
+		);
 };
 
 export default Product;
